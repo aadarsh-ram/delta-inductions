91e1bded96aa1934d528d964b1a8a8acfdd98732
Aadarsh A <aadarsh.ram@gmail.com>
2022-05-26 14:46:25 +0530

    Initial task1

---
+++ task1/permit.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo apt install acl
+cd users
+
+if ! id -u ceo &>/dev/null;
+then
+    sudo useradd ceo -d /
+    echo -e "ceo\nceo\n" | sudo passwd ceo
+fi  
+
+for i in $(ls -d */)
+do
+    branch=${i}
+    cd ${i}
+    for j in $(ls -d */)
+    do
+        user=${j}
+        manager=manager_${branch::-1}
+        sudo chmod -R 550 ./$user
+        sudo setfacl -m u:$manager:rwx ./$user
+        sudo setfacl -m u:$manager:rwx ./$user/Current_Balance.txt
+        sudo setfacl -m u:$manager:rwx ./$user/Transaction_History.txt
+        sudo setfacl -m u:ceo:rwx ./$user
+        sudo setfacl -m u:ceo:rwx ./$user/Current_Balance.txt
+        sudo setfacl -m u:ceo:rwx ./$user/Transaction_History.txt
+    done
+    cd ..
+done
+
+for i in $(ls -d */)
+do
+    branch=${i}
+    sudo chmod 770 ./$branch
+    sudo setfacl -m u:ceo:rwx ./$branch  
+done
+
+echo "All permissions have been changed!"

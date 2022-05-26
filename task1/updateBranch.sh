235038a3247bf9a61e523480e31e3294bb2b3828
Aadarsh A <aadarsh.ram@gmail.com>
2022-05-26 15:30:18 +0530

    Added updateBranch.sh

---
+++ task1/updateBranch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd users
+for i in $(ls -d */)
+do
+    branch=${i}
+    cd ${i}
+    touch Branch_Current_Balance.txt
+    touch Branch_Transaction_History.txt
+    currbal=0
+    for j in $(ls -d */)
+    do
+        user=${j}
+        cd $user
+        num=( $(cat Current_Balance.txt) )
+        currbal=$(($currbal + ${num[0]}))
+        cat ./Transaction_History.txt >> ../Branch_Transaction_History.txt
+        cd ..
+    done
+    echo $currbal >> ./Branch_Current_Balance.txt
+    cd ..
+done

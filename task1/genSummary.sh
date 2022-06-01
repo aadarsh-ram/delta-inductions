f7100489bbc995c3790a327fb7e70bf21e9fe1df
Aadarsh A <aadarsh.ram@gmail.com>
2022-06-01 14:49:44 +0530

    Changes per month WIP

--- task1/genSummary.sh
+++ task1/genSummary.sh
@@ -12,38 +12,53 @@ else
     echo "Defaulting to files/Transacion_List.txt"
 fi
 
-declare -A accounts
-
 while read line; do
     row=( $line )
-    accName=${row[0]}
-    amt=${row[1]}
-    flag=0
-    ind=-1; cnt=0
-    for i in ${!accounts[@]}; 
-    do
-        if [[ $i == $accName ]]; then
-            ind=cnt; flag=1;
-            break;
-        fi
-        ((cnt++))
-    done
-    if [[ flag -eq 1 ]];
-    then
-        newAmt=$(echo ${accounts[$accName]} $amt | awk '{printf "%f", $1 + $2}')
-        accounts[$accName]=$newAmt;
-    else
-        accounts+=([$accName]=500)
-    fi
+    date=${row[2]}
+    month=${date:5:2}
+    mkdir -p ./files/trans
+    touch ./files/trans/Transacion_List_$month.txt
+    echo $line >> ./files/trans/Transacion_List_$month.txt
 done <<<$(sed '1d' $history)
 
-printf "Account changes summary (highest to lowest)\n"
-echo "Account" '   ' "Change"
-for k in "${!accounts[@]}"
+# TODO:
+# Need to take changes each month using two arrays
+# Expenditures stats need to be calculated per month
+transdir=`ls ./files/trans/*.txt`
+for file in $transdir
 do
-  echo $k ' : ' ${accounts["$k"]}
-done | sort -rn -k3
+    declare -A accounts
 
+    while read line; do
+        row=( $line )
+        accName=${row[0]}
+        amt=${row[1]}
+        flag=0
+        ind=-1; cnt=0
+        for i in ${!accounts[@]}; 
+        do
+            if [[ $i == $accName ]]; then
+                ind=cnt; flag=1;
+                break;
+            fi
+            ((cnt++))
+        done
+        if [[ flag -eq 1 ]];
+        then
+            newAmt=$(echo ${accounts[$accName]} $amt | awk '{printf "%f", $1 + $2}')
+            accounts[$accName]=$newAmt;
+        else
+            accounts+=([$accName]=500)
+        fi
+    done <<<$(cat $file)
+
+    printf "Account changes summary (highest to lowest)\n"
+    echo "Account" '   ' "Change"
+    for k in "${!accounts[@]}"
+    do
+    echo $k ' : ' ${accounts["$k"]}
+    done | sort -rn -k3
+done
 
 expfile=./files/allExpenditures.txt
 touch $expfile

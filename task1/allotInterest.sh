a0b32cbf77261cf829b91cf3137e7010b3411090
Aadarsh A <aadarsh.ram@gmail.com>
2022-06-03 21:27:55 +0530

    Added cronjob for allotInterest.sh

--- task1/allotInterest.sh
+++ task1/allotInterest.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Generate a cronjob for this script
+sudo crontab -l > cron_bkp
+sudo echo "0 0 * * * sudo ./allotInterest.sh >/dev/null 2>&1" > cron_bkp
+sudo crontab cron_bkp
+sudo rm cron_bkp
+
 userfile=$1
 
 if [ -f "${userfile}" ]

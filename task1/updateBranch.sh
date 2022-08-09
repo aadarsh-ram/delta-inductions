950f0711a9cd867c71a5a752c3d604b42f31adba
Aadarsh A <aadarsh.ram@gmail.com>
2022-08-10 02:38:08 +0530

    Minor fix

--- task1/updateBranch.sh
+++ task1/updateBranch.sh
@@ -5,8 +5,8 @@ for i in $(ls -d */)
 do
     branch=${i}
     cd ${i}
-    touch ${branch}_Current_Balance.txt
-    touch ${branch}_Transaction_History.txt
+    touch ${branch::-1}_Current_Balance.txt
+    touch ${branch::-1}_Transaction_History.txt
     cp ../../files/Daily_Interest_Rates.txt Daily_Interest_Rates.txt
     currbal=0
     for j in $(ls -d */)
@@ -15,10 +15,10 @@ do
         cd $user
         num=( $(cat Current_Balance.txt) )
         currbal=`echo $currbal ${num[0]} | awk '{printf "%f", $1 + $2}'` 
-        cat ./Transaction_History.txt >> ../${branch}_Transaction_History.txt
+        cat ./Transaction_History.txt >> ../${branch::-1}_Transaction_History.txt
         cd ..
     done
     > ./${branch}_Current_Balance.txt
-    echo $currbal >> ./${branch}_Current_Balance.txt
+    echo $currbal >> ./${branch::-1}_Current_Balance.txt
     cd ..
 done
